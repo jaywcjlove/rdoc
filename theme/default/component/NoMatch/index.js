eb931b3250deb0c407498f101b423bb38861f04b
jaywcjlove <398188662@qq.com>
2018-01-02 23:32:36 +0800

    Update NoMatch component.

--- theme/default/component/NoMatch/index.js
+++ theme/default/component/NoMatch/index.js
@@ -4,10 +4,17 @@ import styles from './index.less';
 
 const Footer = () => {
   return (
-    <div className={styles.noMatch}>
-      页面不存在
-      <Link to="/">返回首页</Link>
-    </div>
+    <table className={styles.noMatch}>
+      <tbody>
+        <tr>
+          <td>
+            <h1>404</h1>
+            <div>杯具啊！页面不存在 </div>
+            <Link className={styles.button} to="/">返回首页</Link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
