44bd01e5352e153f8078f1d5ae5ed4e174ef3a56
jaywcjlove <398188662@qq.com>
2018-03-14 21:17:04 +0800

    Optimized deploy command.

--- src/publish.js
+++ src/publish.js
@@ -1,20 +1,17 @@
 const PATH = require('path');
 const FS = require('fs');
-const gitP = require('simple-git');
-const rimraf = require('rimraf');
+const ghpages = require('gh-pages');
 const loading = require('loading-cli');
 
 module.exports = function server(cmd) {
   // 删除 .git 目录
-  rimraf.sync(PATH.join(cmd.output, '.git'));
+  // rimraf.sync(PATH.join(cmd.output, '.git'));
   if (!FS.existsSync(cmd.output)) {
     console.log(`You need to run the ${`"npm run build"`.yellow} command.`);
     console.log(`The ${(cmd.output + '').red} folder does net exist!\n`);
     return;
-    console.log('cmd.output:', cmd.output, FS.existsSync(cmd.output));
   }
 
-  const git = gitP(cmd.output);
   console.log('  Start public to your git repo'.green)
   console.log(`  ${cmd.publish}\n`.green)
   const load = loading({
@@ -24,18 +21,13 @@ module.exports = function server(cmd) {
     "stream": process.stdout,
   }).start();
 
-  git.init()
-    .add('./*')
-    .commit(`Update website, ${new Date()}!`)
-    .addRemote('origin', cmd.publish)
-    .push(['-f', 'origin', cmd.branch], (err, message) => {
-      load.stop();
-      if (err) {
-        console.log('error:'.red, err);
-      } else {
-        console.log(`\n  Push to ${cmd.branch} success!\n`.green.bold);
-      }
-    });
-
+  ghpages.publish(cmd.output, {
+    branch: cmd.branch,
+    repo: cmd.publish,
+    message: `Update website, ${new Date()}!`
+  }, function name() {
+    load.stop();
+    console.log(`\n  Push to ${cmd.branch} success!\n`.green.bold);
+  });
 }
 
