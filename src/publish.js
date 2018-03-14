555a008d0bb54430d517af7424d21df0804e5e93
jaywcjlove <398188662@qq.com>
2018-03-14 17:32:02 +0800

    Publish command prompt.

--- src/publish.js
+++ src/publish.js
@@ -1,13 +1,20 @@
 const PATH = require('path');
+const FS = require('fs');
 const gitP = require('simple-git');
 const rimraf = require('rimraf');
 const loading = require('loading-cli');
 
 module.exports = function server(cmd) {
+  // 删除 .git 目录
+  rimraf.sync(PATH.join(cmd.output, '.git'));
+  if (!FS.existsSync(cmd.output)) {
+    console.log(`You need to run the ${`"npm run build"`.yellow} command.`);
+    console.log(`The ${(cmd.output + '').red} folder does net exist!\n`);
+    return;
+    console.log('cmd.output:', cmd.output, FS.existsSync(cmd.output));
+  }
 
-  rimraf.sync(PATH.join(cmd.output, '.git'))
   const git = gitP(cmd.output);
-
   console.log('  Start public to your git repo'.green)
   console.log(`  ${cmd.publish}\n`.green)
   const load = loading({
